b2db84f10159f099733167893fdbb7910bf8844b
Leif Gensert <leifg@gmx.de>
2013-12-20 18:53:53 +0000

    implement basic functionality

--- lib/morfo.rb
+++ lib/morfo.rb
@@ -1,5 +1,37 @@
 require 'morfo/version'
 
 module Morfo
-  # Your code goes here...
+  class Base
+    def self.map from, to, &transformation
+      mapping_actions << [from, to, transformation]
+    end
+
+    def self.morf input
+      input.map do |value|
+        mapping_actions.inject({}) do |output, (from, to, transformation)|
+          resulting_value = apply_transformation(
+                              extract_value(value, from),
+                              transformation
+                            )
+          output.merge!(to => resulting_value) if resulting_value
+          output
+        end
+      end
+    end
+
+    private
+    def self.extract_value value, from
+      Array(from).inject(value) do |resulting_value, key|
+        resulting_value[key]
+      end
+    end
+
+    def self.apply_transformation value, transformation
+      transformation ? transformation.call(value) : value
+    end
+
+    def self.mapping_actions
+      @actions ||= []
+    end
+  end
 end
