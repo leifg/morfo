b92f22fea0b60c7d435257483bd0f4895d16ff35
Leif Gensert <leif@propertybase.com>
2013-12-20 18:53:53 +0000

    implement basic functionality

--- Gemfile
+++ Gemfile
@@ -2,3 +2,15 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in morfo.gemspec
 gemspec
+
+group :test, :development do
+  gem 'guard'
+  gem 'guard-rspec'
+  gem 'simplecov'
+  gem 'pry'
+
+  gem 'rb-inotify', require: false
+  gem 'rb-fsevent', require: false
+  gem 'rb-fchange', require: false
+  gem "codeclimate-test-reporter", require: false
+end
---
+++ spec/lib/morfo_spec.rb
@@ -0,0 +1,84 @@
+require 'spec_helper'
+
+describe Morfo::Base do
+  let(:input) do
+    [
+      {
+        title: 'The Walking Dead',
+        channel: 'AMC',
+        watchers: 1337,
+        status: 'running',
+        cast: ['Lincoln, Andrew', 'McBride, Melissa'],
+        ratings: {
+          imdb: 8.7,
+          trakt: 89,
+          rotten_tomatoes: 93,
+        },
+      },
+      {
+        title: 'Breaking Bad',
+        channel: 'AMC',
+        watchers: 72891,
+        status: 'ended',
+        cast: ['Cranston, Bryan', 'Gunn, Anna'],
+        ratings: {
+          imdb: 9.5,
+          trakt: 95,
+          rotten_tomatoes: 100,
+        },
+      }
+    ]
+  end
+
+  describe '#morf' do
+    context '1 to 1 conversion' do
+      subject do
+        class TitleMapper < Morfo::Base
+          map :title, :tv_show_title
+        end
+        TitleMapper
+      end
+
+      it 'maps title correctly' do
+        expected_output = input.map{|v| {tv_show_title: v[:title]} }
+        expect(subject.morf(input)).to eq(expected_output)
+      end
+
+      it 'leaves out nil values in result' do
+        expected_output = [{},{}]
+        modified_input = input.map{|h| h.reject{|k, v| k == :title}}
+        expect(subject.morf(modified_input)).to eq(expected_output)
+      end
+    end
+
+    context '1 to 1 conversion with transformation' do
+      subject do
+        class NumCastMapper < Morfo::Base
+          map :cast, :cast_num do |cast|
+            cast.size
+          end
+        end
+        NumCastMapper
+      end
+
+      it 'calls transformation correctly' do
+        expected_output = input.map{|v| {cast_num: v[:cast].size} }
+        expect(subject.morf(input)).to eq(expected_output)
+      end
+    end
+
+    context 'nested conversion' do
+      subject do
+        class ImdbRatingMapper < Morfo::Base
+          map [:ratings, :imdb], :rating
+        end
+        ImdbRatingMapper
+      end
+
+      it 'maps nested attributes' do
+        expected_output = input.map{|v| {rating: v[:ratings][:imdb]} }
+        expect(subject.morf(input)).to eq(expected_output)
+      end
+    end
+  end
+end
