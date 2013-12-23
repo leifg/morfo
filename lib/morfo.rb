49761354df4bbe85d8e84260f07199bd49b5a4b5
Leif Gensert <leifg@gmx.de>
2013-12-23 02:05:26 +0100

    internal changes for mapping

--- lib/morfo.rb
+++ lib/morfo.rb
@@ -3,35 +3,48 @@ require 'morfo/version'
 module Morfo
   class Base
     def self.map from, to, &transformation
-      mapping_actions << [from, to, transformation]
+      mapping_actions << MapAction.new(from, to, transformation)
     end
 
     def self.morf input
       input.map do |value|
-        mapping_actions.inject({}) do |output, (from, to, transformation)|
-          resulting_value = apply_transformation(
-                              extract_value(value, from),
-                              transformation
-                            )
-          output.merge!(to => resulting_value) if resulting_value
-          output
+        mapping_actions.inject({}) do |output, action|
+          output.merge!(action.execute(value))
         end
       end
     end
 
     private
-    def self.extract_value value, from
+    def self.mapping_actions
+      @actions ||= []
+    end
+  end
+
+  class MapAction
+    attr_reader :from
+    attr_reader :to
+    attr_reader :transformation
+
+    def initialize from, to, transformation
+      @from = from
+      @to = to
+      @transformation = transformation
+    end
+
+    def execute value
+      resulting_value = apply_transformation(extract_value(value))
+      resulting_value ? { to => resulting_value } : {}
+    end
+
+    private
+    def extract_value value
       Array(from).inject(value) do |resulting_value, key|
         resulting_value ? resulting_value[key] : nil
       end
     end
 
-    def self.apply_transformation value, transformation
+    def apply_transformation value
       transformation ? transformation.call(value) : value
-    end
-
-    def self.mapping_actions
-      @actions ||= []
     end
   end
 end
