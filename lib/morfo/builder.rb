216894f02edfa2a90ab5b42b987f7e8825fe2d84
Leif Gensert <leifg@gmx.de>
2015-04-23 13:00:35 -0700

    add possibility to add calculations in nested hashes

--- lib/morfo/builder.rb
+++ lib/morfo/builder.rb
@@ -5,20 +5,23 @@ module Morfo
     end
 
     def build
-      # WTF definitions is not accessable inside class
+      # WTF??? `definitions` is not accessible inside class
       # so this javascript technique is necesseray
       tmp_definitions = definitions.map { |h| h.symbolize_keys }
       Class.new(Morfo::Base) do
         tmp_definitions.each do |definition|
-          f = field(definition[:field])
+          f = field(*definition[:field])
+
           if definition[:from]
-            f = f.from(definition[:from])
+            f = f.from(*definition[:from])
           end
-          if definition[:static]
-            f = f.calculated { definition[:static] }
+
+          if definition[:calculated]
+            f = f.calculated { |r| definition[:calculated] % Morfo::Tools::FlattenHashKeys.new(r).flatten }
           end
-          if definition[:transformation]
-            f = f.transformed { |r| definition[:transformation] % {value: r} }
+
+          if definition[:transformed]
+            f = f.transformed { |v| definition[:transformed] % {value: v} }
           end
         end
       end
