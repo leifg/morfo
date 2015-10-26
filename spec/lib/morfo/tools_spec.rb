b3a9667b6f3df4c6f0cfec3fd5dc0754f0b4acd3
Leif Gensert <leif@propertybase.com>
2015-10-26 14:57:41 -0500

    add tools for creating base keys

--- spec/lib/morfo/tools_spec.rb
+++ spec/lib/morfo/tools_spec.rb
@@ -32,6 +32,19 @@ module Morfo
           end
         end
 
+        context "already nested" do
+          let(:input_hash) { { :"a.nested" => :hash } }
+          let(:expected_output) { { :"a.nested" => :hash } }
+
+          it "returns a new hash" do
+            expect(subject.flatten).not_to be(input_hash)
+          end
+
+          it "returns the correct hash" do
+            expect(subject.flatten).to eq(expected_output)
+          end
+        end
+
         context "multiple nested hash" do
           let(:input_hash) { { a: { deeper: { nested: :hash } } } }
           let(:expected_output) { { :"a.deeper.nested" => :hash } }
@@ -83,6 +96,45 @@ module Morfo
 
           it "returns the correct hash" do
             expect(subject.flatten).to eq(expected_output)
+          end
+        end
+      end
+    end
+
+    describe BaseKeys do
+      subject { described_class.new(input_string) }
+
+      describe "#build" do
+        context "empty string" do
+          let(:input_string) { "" }
+          let(:expected_output) { { } }
+
+          context "empty string" do
+            it "returns empty hash" do
+              expect(subject.build).to eq(expected_output)
+            end
+          end
+        end
+
+        context "nil input" do
+          let(:input_string) { nil }
+          let(:expected_output) { { } }
+
+          context "empty string" do
+            it "returns empty hash" do
+              expect(subject.build).to eq(expected_output)
+            end
+          end
+        end
+
+        context "non nested values" do
+          let(:input_string) { "The rating is: %{rating}" }
+          let(:expected_output) { { rating: nil } }
+
+          describe "build" do
+            it "returns expected values" do
+              expect(subject.build).to eq(expected_output)
+            end
           end
         end
       end
